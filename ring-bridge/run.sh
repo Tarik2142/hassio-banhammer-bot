f47cd4c8c719e4787b79439aaa859194811272e3
Tarik2142 <31830530+Tarik2142@users.noreply.github.com>
2024-12-04 18:55:17 +0200

    update addon info

--- ring-bridge/run.sh
+++ ring-bridge/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 set +u
 
-export REFRESH_TOKEN=$(bashio::config 'refresh_token')
-bashio::log.info "Refresh token configured as ${REFRESH_TOKEN}."
+export BOT_TOKEN=$(bashio::config 'bot_token')
+bashio::log.info "Bot token configured to ${BOT_TOKEN}."
 
-bashio::log.info "Starting bridge service."
+bashio::log.info "Starting ban service."
 npm run start
